8564675896308ba76ed86cb3243e02c5f3c3ea7c
Marior9 <mariorodriguezan@gmail.com>
2019-07-01 20:27:31 -0400

    uploader added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,141 +1,109 @@
 import React from 'react'
 import Images from './Images'
 import { Card } from 'semantic-ui-react'
+import {Image,CloudinaryContext} from 'cloudinary-react';
+import Categories from './Categories';
 
 export default class Profile extends React.Component{
     constructor(props) {
         super(props)
         this.state = { 
             hightlight: false,
-            data:[],
-            image: []
+            scores:[],
+            data: []
         }
-
-        this.fileInputRef = React.createRef()
-    
-        this.openFileDialog = this.openFileDialog.bind(this)
-        this.onFilesAdded = this.onFilesAdded.bind(this)
-        this.onDragOver = this.onDragOver.bind(this)
-        this.onDragLeave = this.onDragLeave.bind(this)
-        this.onDrop = this.onDrop.bind(this)
-      }
-    
-      openFileDialog() {
-        if (this.props.disabled) return
-        this.fileInputRef.current.click()
-      }
-    
-      onFilesAdded(evt) {
-        let img = evt.target.files
-        var imageData = new FormData()
-       
-        for (const file of evt.target.files) {
-            imageData.append('files',file,file.name)
-        }
-       
-        
-        // if (this.props.disabled) return
-        // const files = evt.target.files
-        // if (this.props.onFilesAdded) {
-        //   const array = this.fileListToArray(files)
-        //   this.props.onFilesAdded(array)
-        // }
-        fetch('http://localhost:3000/api/v1/users',{
-            method: "POST",
-            body: imageData
-           	}).then(res=>res.json()).then(data => {
-//   debugger
-                this.handleFetchResponse(data)})
+      
+    }
             
-        }
-    
-      handleFetchResponse=(data)=>{
-          console.log(data)
-          debugger
-          this.setState({
-              data: data.result.images[0].classifiers[0].classes.filter(img => img.score < 1.00 && img.score > 0.9), //.sort((a, b) => (a.score < b.score) ? 1 : -1)
-              image: data
+showUploadWidget=()=> {
+    window.cloudinary.openUploadWidget({
+       cloudName: "translateme",
+       uploadPreset: "qks45ycm",
+       sources: [
+           "local",
+           "dropbox",
+           "camera",
+           "facebook",
+           "instagram",
+           "url"
+       ],
+       showAdvancedOptions: false,
+       cropping: false,
+       multiple: true,
+       singleUploadAutoClose: false,
+       defaultSource: "local",
+       styles: {
+           palette: {
+               window: "#F5F5F5",
+               sourceBg: "#FFFFFF",
+               windowBorder: "#90a0b3",
+               tabIcon: "#0094c7",
+               inactiveTabIcon: "#69778A",
+               menuIcons: "#0094C7",
+               link: "#53ad9d",
+               action: "#8F5DA5",
+               inProgress: "#0194c7",
+               complete: "#53ad9d",
+               error: "#c43737",
+               textDark: "#000000",
+               textLight: "#FFFFFF"
+           },
+           fonts: {
+               default: null,
+               "'IBM Plex Sans', sans-serif": {
+                   url: "https://fonts.googleapis.com/css?family=IBM+Plex+Sans",
+                   active: true
+               }
+           }
+       }
+       
+   },
+   
+    (err, info) => {
+      if (!err) {   
+        
+        console.log("Upload Widget event - ", info);
+        if(info.event === "success"){
+            console.log("URL " + info.info.url)
+        fetch('http://localhost:3000/api/v1/users',{
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({
+            image: info.info.url
             })
-        // var imageData = new FormData()
-       
-        // for (const file of evt.target.files) {
-        //     imageData.append('files',file,file.name)
-        // }
-       
-
-      }  
-
-
-      onDragOver(evt) {
-        evt.preventDefault()
-    
-        if (this.props.disabled) return
-    
-        this.setState({ hightlight: true })
-      }
-    
-      onDragLeave() {
-         
-        this.setState({ hightlight: false })
-      }
-    
-      onDrop(event) {
-        event.preventDefault()
-    
-        if (this.props.disabled) return
-    
-        const files = event.dataTransfer.files
-        if (this.props.onFilesAdded) {
-          const array = this.fileListToArray(files)
-          this.props.onFilesAdded(array)
-        }
-        this.setState({ hightlight: false })
-      }
-    
-      fileListToArray(list) {
-        const array = []
-        for (var i = 0; i < list.length; i++) {
-          array.push(list.item(i))
-        }
+        })
+        .then(res=>res.json()).then(data => {
+            // debugger
  
-        return array
+        this.handleFetchResponse(data)})
+        }
+
       }
-
-
+     });
+     
+    }
+    handleFetchResponse=(data)=>{
+        console.log(data)
+        // debugger
+        this.setState({
+            scores: data.result.images[0].classifiers[0].classes.filter(img => img.score < 1.00 && img.score > 0.9), //.sort((a, b) => (a.score < b.score) ? 1 : -1)
+            data: data
+          })
+   
+    }
     render(){
+
+       
+ 
         return(
             <div>
-                <div
+                <Card.Group>
+                    <button raised className="card" id="upload_widget" onClick={()=>{this.showUploadWidget()}} class="cloudinary-button">Upload files</button>
+
                 
-                className={`Dropzone ${this.state.hightlight ? "Highlight" : ""}`}
-                onDragOver={this.onDragOver}
-                onDragLeave={this.onDragLeave}
-                onDrop={this.onDrop}
-                onClick={this.openFileDialog}
-                style={{ cursor: this.props.disabled ? "default" : "pointer" }}
-                >
-                <input
-                ref={this.fileInputRef}
-                className="FileInput"
-                type="file"
-                multiple
-                onChange={this.onFilesAdded}
-                />
-                <img
-                alt="upload"
-                className="Icon"
-                src="upload-icon-3.png"
-                />
-                <span>Upload Files</span>
-                
+                    <Categories image={this.state.data} scores={this.state.scores}/>
+                </Card.Group>
             </div>
-            <div >
-                    <Card.Group itemsPerRow={6}>
-                       {this.state.data.map(image => <Images pic={this.state.image} image={image}/>)}
-                       
-                    </Card.Group>
-                </div>
-        </div>
         )
     }
 }
